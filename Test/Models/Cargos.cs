08cad3aae28ac169a7567be336e2a3888dded698
Tutuwo <32764102+Tutuwo@users.noreply.github.com>
2019-11-25 10:40:26 +0000

    -commit01

---
+++ Test/Models/Cargos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test.Models
+{
+    public class cargos
+    {
+        public int IDCargo { get; set; }
+
+        public string NomeCargo { get; set; }
+
+        public int RankHierarquia  { get; set; }
+
+        public string descricao { get; set; }
+
+        }
+}
---
+++ Test/Models/Funcionarios.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Test.Models
+{
+    public class funcionarios
+    {
+        public int IDFunc { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DataNasc { get; set; }
+
+        public string sexo { get; set; }
+
+        public int IDCargo { get; set; }
+
+        public int IDDepartamento { get; set; }
+
+        public int IDHorario { get; set; }
+
+    }
+}
